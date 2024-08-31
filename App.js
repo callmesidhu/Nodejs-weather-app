19d547491f6c12658846dd10df0eab8bcfb43c7a
callmesidhu <sidharthpunalur@gmail.com>
2024-08-31 14:26:27 +0530

    work completed

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const {API_KEY} = require('./config.js')
+const axios = require('axios')
 
 app.set("view engine", "ejs");
 
@@ -12,16 +14,29 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/weather", (req, res) => {
+app.get("/weather", async(req, res) => {
 
   const city = req.query.city;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&unites=imperial&appid=${API_KEY}`;
+  let weather ;
+  let error = null;
+
+  try{
+    const response = await axios.get(API_URL)
+    weather = response.data;
+    console.log(weather)
+  }catch(error){
+    weather = null;
+    error = "Error , please try again"
+    console.log(error)
+  }
 
 
-  res.render("index", { weather: null, error: null });
+  res.render("index", { weather, error });
 });
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
---
+++ config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'd61d9b81b936dd718d3b0f6f86c12f35';
+
+module.exports ={API_KEY};
